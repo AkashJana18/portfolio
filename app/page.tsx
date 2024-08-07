bbdc3ec2cb9ae29ff84e1ac9c4f3bb66a76085f4
AkashJana18 <akashjana663@gmail.com>
2024-08-07 13:56:55 +0530

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className="bg-black relative">
-        Hello Portfolio
+    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
