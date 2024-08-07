d1a0996f4aee1821396c26ad001229b81df5f25e
AkashJana18 <akashjana663@gmail.com>
2024-08-07 14:33:21 +0530

    updating page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav 
+        navItems={[{name: "Home", link: '/', icon: <FaHome />}]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
