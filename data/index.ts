6080dfec9bc4d08e13e0092809bf04f9e21a934e
AkashJana18 <akashjana663@gmail.com>
2024-08-16 14:44:53 +0530

    minor change

--- data/index.ts
+++ data/index.ts
@@ -9,7 +9,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "I prioritize team/client collaboration, fostering open communication ",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
